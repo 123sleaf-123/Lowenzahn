e47ef87831c38f9e9800cf65cd8735b795d7022f
jason jx wang <1034696975@qq.com>
2022-07-07 20:16:19 +0800

    增加货物查询前端

---
+++ src/webapp/LwenzahnWeb/src/views/layoutpages/management/Goods.vue
@@ -0,0 +1,16 @@
+<template>
+  <GoodTable></GoodTable>
+</template>
+
+<script>
+import GoodTable from "./components/GoodTable";
+export default {
+  name: "goods",
+  components: {
+    GoodTable,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/webapp/LwenzahnWeb/src/views/layoutpages/management/components/GoodTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <div style="text-align: -webkit-center">
+    <el-container>
+      <el-header>
+        <h1>Goods</h1>
+      </el-header>
+      <el-main>
+        <el-button-group>
+          <el-button type="primary" @click="getGoods" icon="MessageBox">Show</el-button>
+        </el-button-group>
+        <el-table :data="goods" style="width: 100%">
+          <el-table-column prop="gid" label="Good id"></el-table-column>
+          <el-table-column prop="goodName" label="Good Name"></el-table-column>
+          <el-table-column prop="type" label="Type"></el-table-column>
+          <el-table-column prop="shelfid" label="Shelfid"></el-table-column>
+          <el-table-column prop="floor" label="Floor"></el-table-column>
+          <el-table-column prop="start" label="Start"></el-table-column>
+          <el-table-column prop="end" label="End"></el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "GoodTable",
+  components: {
+  },
+  data() {
+    return {
+      goods: [],
+    }
+  },
+  methods: {
+    getGoods() {
+      axios({
+        url: "http://localhost:8080/good",
+        method: 'GET',
+      }).then((res) => {
+        console.log(res.data);
+        this.goods = res.data;
+      });
+    },
+
+  },
+};
+</script>
+
+<style>
+</style>
