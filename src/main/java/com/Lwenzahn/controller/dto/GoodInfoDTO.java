cb76ec105d1d2cc1154d5301641ebe186f71a6ab
sleaf <huangzhiye2002@163.com>
2022-10-14 16:32:12 +0800

    修改了数据库，与物联数据库合并，并且完善了逻辑

--- src/main/java/com/Lwenzahn/controller/dto/GoodInfoDTO.java
+++ src/main/java/com/Lwenzahn/controller/dto/GoodInfoDTO.java
@@ -6,9 +6,9 @@ import lombok.Data;
 public class GoodInfoDTO {
     private String goodsName;
     private String goodsType;
-    private String warehouseId;
-    private String rowNum;
-    private String columnNum;
-    private String floorNum;
-    private String positionNum;
+    private Integer warehouseId;
+    private String shelfId;
+    private Integer positionCode;
+    private Integer rowPosition;
+    private Integer columnPosition;
 }
--- src/main/java/com/Lwenzahn/mapper/WarehouseMapper.java
+++ src/main/java/com/Lwenzahn/mapper/WarehouseMapper.java
@@ -12,28 +12,24 @@ import java.util.List;
 @CrossOrigin
 @Mapper
 public interface WarehouseMapper extends BaseMapper<Warehouse> {
-    @Select("SELECT\n" +
-            "g.goods_Name,\n" +
+    @Select("select \n" +
+            "g.goods_name,\n" +
             "g.goods_type,\n" +
             "w.warehouse_id,\n" +
-            "s.row_num,\n" +
-            "s.column_num,\n" +
-            "sto.floor_num,\n" +
-            "sto.position_num\n" +
-            "FROM\n" +
+            "s.shelf_id,\n" +
+            "s.position_code,\n" +
+            "g.row_position,\n" +
+            "g.column_position\n" +
+            "from \n" +
             "goods g,\n" +
             "shelf s,\n" +
-            "storage sto,\n" +
-            "warehouse w\n" +
-            "WHERE\n" +
-            "w.warehouse_id = #{id}\n" +
-            "AND\n" +
-            "g.goods_id = sto.goods_id\n" +
-            "AND\n" +
-            "sto.shelf_id = s.shelf_id\n" +
-            "AND\n" +
-            "w.warehouse_id = s.warehouse_id\n" +
-            "AND\n" +
-            "sto.is_out_storage = 0;")
-    List<GoodInfoDTO> findGoodInfo(@Param("id")Integer whid);
+            "warehouse w,\n" +
+            "storage s2\n" +
+            "where \n" +
+            "w.warehouse_id = #{id} and " +
+            "g.is_storage = true and \n" +
+            "s2.goods_id = g.goods_id and \n" +
+            "s2.shelf_id = s.shelf_id and \n" +
+            "s.warehouse_id = w.warehouse_id;")
+    List<GoodInfoDTO> findGoodInfo(@Param("id")Integer warehouseId);
 }
